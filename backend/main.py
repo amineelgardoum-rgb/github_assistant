b23ed5eee57db89fb72fcc96cc3062c1c12abcf2
amine <amineotako64@gmail.com>
2025-12-25 19:16:54 +0100

    Changes to be committed: 	modified:   backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -65,4 +65,4 @@ def ask_question(req: AskRequest):
     return {"answer": answer, "sources": sources}
 
 if __name__=="__main__":
-    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True)
+    uvicorn.run("main:app",host="127.0.0.1",port=8000,reload=True)
